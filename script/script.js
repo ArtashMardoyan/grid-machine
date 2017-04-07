4f11359c7a6154910f09e3e93c0be6527e3a213b
artash <artashmardoyan@gmail.com>
2017-04-07 15:30:58 +0400

    colorize grid

--- script/script.js
+++ script/script.js
@@ -1,55 +1,102 @@
-"use strict";
+'use strict';
 
 (function IIFE() {
 
     var board = {
-        tableForm: null,
-        coordinateOfTable: null,
-        title: null
+        form: null,
+        container: null,
+        title: null,
+        colorizeBtn: null,
+        inlineDiv: null,
+        containDiv: null,
+        countInline: 0,
+        countContain: 1
     };
 
     function init() {
-        board.tableForm = document.getElementById("tableForm");
-        board.coordinateOfTable = document.getElementById("coordinate");
-        board.title = document.getElementById("title");
+        board.form = document.getElementById('tableForm');
+        board.container = document.getElementById('coordinate');
+        board.title = document.getElementById('title');
+        board.colorizeBtn = document.createElement('input');
+        board.inlineDiv = document.getElementsByClassName('divInLine');
+        board.containDiv = document.getElementsByClassName('containerDiv');
         bindEvents();
     }
 
     function bindEvents() {
-        if (board.tableForm.addEventListener) {
-            board.tableForm.addEventListener("submit", drawGrid)
-        } else if (board.tableForm.attachEvent) {
-            board.tableForm.attachEvent('onsubmit', drawGrid);
+        if (board.form) {
+            if (board.form.addEventListener)
+                board.form.addEventListener('submit', drawGrid);
+            else if (board.form.attachEvent)
+                board.form.attachEvent('onsubmit', drawGrid);
+        }
+        if (board.colorizeBtn) {
+            if (board.colorizeBtn.addEventListener)
+                board.colorizeBtn.addEventListener('click', colorize)
         }
     }
 
-    function clearTable() {
-        board.coordinateOfTable.innerHTML = "";
-        var inInput = document.getElementById("inputNum");
-        inInput.value = "";
+    function clearGrid() {
+        board.container.innerHTML = '';
+    }
+
+    function clearInput(input) {
+        input.value = '';
     }
 
     function drawGrid(event) {
         event.preventDefault();
-        var parsedNum = parseInt(document.getElementById("inputNum").value);
+        var inputElem = document.getElementById('inputNum');
+        var parsedNum = parseInt(inputElem.value);
         if (isNaN(parsedNum)) {
-            alert("Please enter only number");
+            alert('Please enter only number');
             return;
         }
-        if (parsedNum != "" || board.coordinateOfTable.innerHTML != "") {
-            clearTable();
-        }
+        clearGrid();
         for (var i = 0; i < parsedNum; i++) {
+            var lineDiv = document.createElement('div');
+            board.container.appendChild(lineDiv);
+            lineDiv.className = 'containerDiv';
             for (var j = 0; j < parsedNum; j++) {
-                var lineDiv = document.createElement("div");
-                board.coordinateOfTable.appendChild(lineDiv);
-                if ((i + j) % 2 == 0) lineDiv.className = "divWhite";
-                else lineDiv.className = "divBlack";
+                lineDiv.innerHTML += '<div class="divInLine"></div>';
             }
-            var br = document.createElement("br");
-            board.coordinateOfTable.appendChild(br)
         }
+        board.form.appendChild(board.colorizeBtn);
+        board.colorizeBtn.type = 'button';
+        board.colorizeBtn.value = 'Colorize';
+        clearInput(inputElem);
+
+    }
+
+    function colorize() {
+        board.colorizeBtn.disabled = true;
+        document.getElementById('draw').disabled = true;
+        if (board.countContain > board.containDiv.length) {
+            board.colorizeBtn.disabled = false;
+            document.getElementById('draw').disabled = false;
+            board.countInline = 0;
+            board.countContain = 1;
+            return;
+        }
+        if (board.countInline < board.inlineDiv.length) {
+            board.inlineDiv[board.countInline].style.backgroundColor = randomColor();
+            board.countInline += board.containDiv.length;
+            setTimeout(colorize, 500);
+            if (board.countInline >= board.inlineDiv.length) {
+                board.countInline = board.countContain;
+                board.countContain++;
+            }
+        }
+    }
+
+    function randomColor() {
+        var red = Math.floor(Math.random() * 256);
+        var green = Math.floor(Math.random() * 256);
+        var blue = Math.floor(Math.random() * 256);
+        var colorful = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        return colorful;
     }
 
     init();
 })();
+
